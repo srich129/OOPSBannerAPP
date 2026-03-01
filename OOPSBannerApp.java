8d0dcebd4454d8a9d5f9c9fa420956a5694b989e
et sree charitha <yourname@example.com>
2026-03-01 18:57:20 +0530

    UC2 - OOPS Banner App - Print Banner Feature

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,7 +1,14 @@
 public class OOPSBannerApp {
- public static void main (String[] 
- args) {
-         System.out.print1n("OOPS");
-      }
 
+    public static void main(String[] args) {
+
+        System.out.println(" *****   *****   *****   ***** ");
+        System.out.println("*     * *     * *     * *     *");
+        System.out.println("*     * *     * *     * *      ");
+        System.out.println("*     * *     * *     *  ***** ");
+        System.out.println("*     * *     * *     *       *");
+        System.out.println("*     * *     * *     * *     *");
+        System.out.println(" *****   *****   *****   ***** ");
+
+    }
 }
